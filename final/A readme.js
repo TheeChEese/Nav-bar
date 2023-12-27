ea9f4833e2503bfe8f5cd55b2fe4d25a983a61ad
Bri <113311495+TheeChEese@users.noreply.github.com>
2023-12-26 19:46:49 -0800

    Add files via upload

---
+++ final/A readme.js
@@ -0,0 +1,5 @@
+//Modification #1 Create an additional dropdown menu for the “Home” section
+
+//Modification #2 Create an additional dropdown for the “About” section
+
+//Modification #3 Create an additional dropdown for the “Projects”
